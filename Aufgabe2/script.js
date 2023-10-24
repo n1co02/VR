9a71f8f38be8f3dd8db6c400d5ed69de1c7807dd
n1co02 <nicolasostermann@web.de>
2023-10-24 14:53:02 +0200

    make animation reusable

--- Aufgabe2/script.js
+++ Aufgabe2/script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("startButton").addEventListener("click", function () {
-    document.querySelector(".car").classList.add("car-moving");
+    const carElement = document.querySelector(".car");
+    // Remove the car-moving class to reset the animation
+    carElement.classList.remove("car-moving");
+    // Using setTimeout to force browser reflow and restart the animation
+    setTimeout(() => {
+      carElement.classList.add("car-moving");
+    }, 10); // 10ms is just a short delay to ensure the browser has time to reflow
   });
+
   document.getElementById("clearButton").addEventListener("click", function () {
     const timeInSeconds = parseInt(document.getElementById("timeInput").value);
     document.body.style.transitionDuration = `${timeInSeconds}s`;
